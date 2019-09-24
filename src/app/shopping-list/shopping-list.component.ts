ccd2b365e5526439104a0392da029b4ff1b556e0
Luka <l.bjelica@globalsoft.ba>
2019-09-24 10:40:40 +0200

    Using Subjects instead EventEmitters

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "./shopping-list.service";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[];
+export class ShoppingListComponent implements OnInit, OnDestroy {
+    ingredients: Ingredient[];
+    private igChangeSub: Subscription;
 
-  constructor(private shoppingListService: ShoppingListService) { }
+    constructor(private shoppingListService: ShoppingListService) {
+    }
 
-  ngOnInit() {
-      this.ingredients = this.shoppingListService.getIngredients();
-      this.shoppingListService.ingredientChanged
-          .subscribe(
-              (ingredients: Ingredient[]) => {
-                this.ingredients = ingredients;
-              }
-          );
-  }
+    ngOnInit() {
+        this.ingredients = this.shoppingListService.getIngredients();
+        this.igChangeSub = this.shoppingListService.ingredientChanged
+            .subscribe(
+                (ingredients: Ingredient[]) => {
+                    this.ingredients = ingredients;
+                }
+            );
+    }
+
+    ngOnDestroy(): void {
+        this.igChangeSub.unsubscribe();
+    }
 }
