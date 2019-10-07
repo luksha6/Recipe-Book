4251b22156a23eb1873e3ef53a8bc1c534f13886
Luka <l.bjelica@globalsoft.ba>
2019-10-07 11:10:01 +0200

    Shopping list form

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,7 +3,8 @@ import {Ingredient} from "../shared/ingredient.model";
 import { Subject } from "rxjs";
 
 export class ShoppingListService {
-    ingredientChanged = new Subject<Ingredient[]>()
+    ingredientChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -15,6 +16,10 @@ export class ShoppingListService {
         return this.ingredients.slice();
     }
 
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
         this.ingredientChanged.next(this.ingredients.slice());
@@ -23,5 +28,15 @@ export class ShoppingListService {
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
         this.ingredientChanged.next(this.ingredients);
+    }
+
+    updateIngredients(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
+        this.ingredientChanged.next(this.ingredients.slice());
     }
 }
