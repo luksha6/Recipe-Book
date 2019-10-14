2a15f616349275605d88f002c6be4c1228512780
Luka <l.bjelica@globalsoft.ba>
2019-10-14 12:27:34 +0200

    Implemented lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
 
 const routes: Routes = [
-    {path: '', redirectTo: '/recipes', pathMatch: 'full'}
+    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'}, // loadChildren -> only load component when user visit that path :: RecipesModule name of the Class inside file recipes.module.ts :: LazyLoading
+    {path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'},
+    {path: 'auth', loadChildren: './auth/auth.module#AuthModule'}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})], // PreloadAllModules (default is no)
     exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -10,7 +10,7 @@ import {FormsModule} from "@angular/forms";
     ],
     imports: [
         RouterModule.forChild([
-            {path: 'auth', component: AuthComponent},
+            {path: '', component: AuthComponent},
         ]),
         SharedModule,
         FormsModule
